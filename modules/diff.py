7d2e17e0513ac9dae69e4c21a8b04593f8d1e13c
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-10-10 17:30:53 -0400

    Streamline Diff Module

--- modules/diff.py
+++ modules/diff.py
@@ -1,43 +1,50 @@
 import os
+from typing import Callable
 from modules.apt_att_file import APT_ATT_File
 from modules.apt_base_file import APT_BASE_File
 from modules.apt_con_file import APT_CON_File
 from modules.apt_rmk_file import APT_RMK_File
 from modules.apt_rwy_file import APT_RWY_File
 from modules.apt_rwy_end_file import APT_RWY_END_File
+from modules.faa_file_base import FAA_File_Base
 from modules.filters import Filters
 from modules.navdata_handler import get_csv_files
 from modules.reports_handler import REPORTS_DIR
 
 FILE_SUFFIX = "CHG_RPT.csv"
 
+FILE_TYPES = {
+    "APT_ATT": APT_ATT_File,
+    "APT_BASE": APT_BASE_File,
+    "APT_CON": APT_CON_File,
+    "APT_RMK": APT_RMK_File,
+    "APT_RWY": APT_RWY_File,
+    "APT_RWY_END": APT_RWY_END_File,
+}
+
+OUTPUT_NAMES = {
+    "APT_ATT": "APT_ATT.txt",
+    "APT_BASE": "APT_BASE.txt",
+    "APT_CON": "APT_CON.txt",
+    "APT_RMK": "APT_RMK.txt",
+    "APT_RWY": "APT_RWY.txt",
+    "APT_RWY_END": "APT_RWY_END.txt",
+}
+
 
 class Diff:
     format: str
     filters: Filters | None
     file_paths: list[str]
-    apt_att: APT_ATT_File | None
-    apt_base: APT_BASE_File | None
-    apt_con: APT_CON_File | None
-    apt_rmk: APT_RMK_File | None
-    apt_rwy: APT_RWY_File | None
-    apt_rwy_end: APT_RWY_END_File | None
+    files_map: dict[str, FAA_File_Base]
 
     def __init__(self, format: str, should_show: bool, use_filters: bool) -> None:
         self.format = format
         self.filters = None
         self.file_paths = get_csv_files()
-        self.apt_att = None
-        self.apt_base = None
-        self.apt_con = None
-        self.apt_rmk = None
-        self.apt_rwy = None
-        self.apt_rwy_end = None
+        self.files_map = {}
 
-        if use_filters:
-            self.__process_filtered_file_list(should_show)
-        else:
-            self.__process_file_list()
+        self.__process_file_list(should_show, use_filters)
 
     def build_reports(self) -> None:
         if self.format == "console":
@@ -45,94 +52,38 @@ class Diff:
         if self.format == "text":
             self.__to_text_report()
 
-    def __process_filtered_file_list(self, should_show: bool) -> None:
-        self.filters = Filters(should_show)
-        airports = self.filters.airports if len(self.filters.airports) > 0 else None
+    def __process_file_list(self, should_show: bool, use_filters: bool) -> None:
+        airports = None
+        allowed = set(FILE_TYPES.keys())
+
+        if use_filters:
+            self.filters = Filters(should_show)
+            airports = self.filters.airports or None
+            if self.filters.files:
+                allowed = set(self.filters.files)
 
         for fp in self.file_paths:
-            if len(self.filters.files) > 0:
-                if (
-                    fp.endswith(f"APT_ATT_{FILE_SUFFIX}")
-                    and "APT_ATT" in self.filters.files
-                ):
-                    self.apt_att = APT_ATT_File(fp, airports)
-                if (
-                    fp.endswith(f"APT_BASE_{FILE_SUFFIX}")
-                    and "APT_BASE" in self.filters.files
-                ):
-                    self.apt_base = APT_BASE_File(fp, airports)
-                if (
-                    fp.endswith(f"APT_CON_{FILE_SUFFIX}")
-                    and "APT_CON" in self.filters.files
-                ):
-                    self.apt_con = APT_CON_File(fp, airports)
-                if (
-                    fp.endswith(f"APT_RMK_{FILE_SUFFIX}")
-                    and "APT_RMK" in self.filters.files
-                ):
-                    self.apt_rmk = APT_RMK_File(fp, airports)
-                if (
-                    fp.endswith(f"APT_RWY_{FILE_SUFFIX}")
-                    and "APT_RWY" in self.filters.files
-                ):
-                    self.apt_rwy = APT_RWY_File(fp, airports)
-                if (
-                    fp.endswith(f"APT_RWY_END_{FILE_SUFFIX}")
-                    and "APT_RWY_END" in self.filters.files
-                ):
-                    self.apt_rwy_end = APT_RWY_END_File(fp, airports)
+            for key, rpt in FILE_TYPES.items():
+                if key in allowed and fp.endswith(f"{key}_{FILE_SUFFIX}"):
+                    if airports is not None:
+                        self.files_map[key] = rpt(fp, airports)
+                    else:
+                        self.files_map[key] = rpt(fp)
+                    break
 
-    def __process_file_list(self) -> None:
-        for fp in self.file_paths:
-            if fp.endswith(f"APT_ATT_{FILE_SUFFIX}"):
-                self.apt_att = APT_ATT_File(fp)
-            if fp.endswith(f"APT_BASE_{FILE_SUFFIX}"):
-                self.apt_base = APT_BASE_File(fp)
-            if fp.endswith(f"APT_CON_{FILE_SUFFIX}"):
-                self.apt_con = APT_CON_File(fp)
-            if fp.endswith(f"APT_RMK_{FILE_SUFFIX}"):
-                self.apt_rmk = APT_RMK_File(fp)
-            if fp.endswith(f"APT_RWY_{FILE_SUFFIX}"):
-                self.apt_rwy = APT_RWY_File(fp)
-            if fp.endswith(f"APT_RWY_END_{FILE_SUFFIX}"):
-                self.apt_rwy_end = APT_RWY_END_File(fp)
+    def __handle_operation(self, operation: Callable) -> None:
+        for key, faa_file in self.files_map.items():
+            if faa_file is not None:
+                operation(key, faa_file)
 
     def __get_text_report(self) -> None:
-        if self.apt_att is not None:
-            print(self.apt_att.get_text_report())
-        if self.apt_base is not None:
-            print(self.apt_base.get_text_report())
-        if self.apt_con is not None:
-            print(self.apt_con.get_text_report())
-        if self.apt_rmk is not None:
-            print(self.apt_rmk.get_text_report())
-        if self.apt_rwy is not None:
-            print(self.apt_rwy.get_text_report())
-        if self.apt_rwy_end is not None:
-            print(self.apt_rwy_end.get_text_report())
+        self.__handle_operation(lambda _, op: print(op.get_text_report()))
 
     def __to_text_report(self) -> None:
-        if self.apt_att is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_ATT.txt")
+        def writer(key: str, faa_file: FAA_File_Base):
+            file_name = OUTPUT_NAMES.get(key, f"{key}.txt")
+            full_path = os.path.join(REPORTS_DIR, file_name)
             with open(full_path, "w") as f:
-                f.writelines(self.apt_att.get_text_report())
-        if self.apt_base is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_BASE.txt")
-            with open(full_path, "w") as f:
-                f.writelines(self.apt_base.get_text_report())
-        if self.apt_con is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_CON.txt")
-            with open(full_path, "w") as f:
-                f.writelines(self.apt_con.get_text_report())
-        if self.apt_rmk is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_RMK.txt")
-            with open(full_path, "w") as f:
-                f.writelines(self.apt_rmk.get_text_report())
-        if self.apt_rwy is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_RWY.txt")
-            with open(full_path, "w") as f:
-                f.writelines(self.apt_rwy.get_text_report())
-        if self.apt_rwy_end is not None:
-            full_path = os.path.join(REPORTS_DIR, "APT_RWY_END.txt")
-            with open(full_path, "w") as f:
-                f.writelines(self.apt_rwy_end.get_text_report())
+                f.writelines(faa_file.get_text_report())
+
+        self.__handle_operation(writer)
