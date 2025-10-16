f49befcafaf61fdae55d7d67666fd3838df9c313
Kyle Rodgers <65290193+misterrodg@users.noreply.github.com>
2025-10-16 08:17:46 -0400

    Add NAV_RMK Handling

--- modules/faa_files/__init__.py
+++ modules/faa_files/__init__.py
@@ -47,6 +47,7 @@ from .mtr_terr_file import MTR_TERR_File
 from .mtr_wdth_file import MTR_WDTH_File
 from .nav_base_file import NAV_BASE_File
 from .nav_ckpt_file import NAV_CKPT_File
+from .nav_rmk_file import NAV_RMK_File
 from .faa_file_base import FAA_File_Base
 
 __all__ = [
@@ -99,5 +100,6 @@ __all__ = [
     "MTR_WDTH_File",
     "NAV_BASE_File",
     "NAV_CKPT_File",
+    "NAV_RMK_File",
     "FAA_File_Base",
 ]
---
+++ modules/faa_files/nav_rmk_file.py
@@ -0,0 +1,162 @@
+from .faa_file_base import FAA_Record_Base, FAA_File_Base
+from modules.action import Action
+from modules.filters import FilterObject
+from modules.record_helpers import replace_empty_string
+from modules.registry import register_faa_file
+
+from typing import Self
+
+import csv
+
+
+class NAV_RMK(FAA_Record_Base):
+    eff_date: str
+    nav_id: str
+    nav_type: str
+    state_code: str
+    city: str
+    country_code: str
+    tab_name: str
+    ref_col_name: str
+    ref_col_seq_no: str
+    remark: str
+
+    def __init__(
+        self,
+        eff_date: str,
+        nav_id: str,
+        nav_type: str,
+        state_code: str,
+        city: str,
+        country_code: str,
+        tab_name: str,
+        ref_col_name: str,
+        ref_col_seq_no: str,
+        remark: str,
+        file: str,
+        action: Action,
+        mods: str,
+    ) -> None:
+        super().__init__(file, action, mods)
+
+        self.eff_date = replace_empty_string(eff_date)
+        self.nav_id = replace_empty_string(nav_id)
+        self.nav_type = replace_empty_string(nav_type)
+        self.state_code = replace_empty_string(state_code)
+        self.city = replace_empty_string(city)
+        self.country_code = replace_empty_string(country_code)
+        self.tab_name = replace_empty_string(tab_name)
+        self.ref_col_name = replace_empty_string(ref_col_name)
+        self.ref_col_seq_no = replace_empty_string(ref_col_seq_no)
+        self.remark = replace_empty_string(remark)
+
+    def __hash__(self) -> int:
+        return hash((self.nav_id, self.nav_type, self.ref_col_seq_no))
+
+    def __eq__(self, other: Self) -> bool:
+        if not isinstance(other, NAV_RMK):
+            return False
+        return (
+            self.nav_id == other.nav_id
+            and self.nav_type == other.nav_type
+            and self.ref_col_seq_no == other.ref_col_seq_no
+        )
+
+    def __lt__(self, other: Self) -> bool:
+        if not isinstance(other, NAV_RMK):
+            return False
+        return (self.nav_id, self.nav_type, self.ref_col_seq_no, self.file) < (
+            other.nav_id,
+            other.nav_type,
+            other.ref_col_seq_no,
+            other.file,
+        )
+
+    def __repr__(self):
+        return (
+            f"{self.__class__.__name__} ( "
+            f"EFF_DATE={self.eff_date!r}, "
+            f"NAV_ID={self.nav_id!r}, "
+            f"NAV_TYPE={self.nav_type!r}, "
+            f"STATE_CODE={self.state_code!r}, "
+            f"CITY={self.city!r}, "
+            f"COUNTRY_CODE={self.country_code!r}, "
+            f"TAB_NAME={self.tab_name!r}, "
+            f"REF_COL_NAME={self.ref_col_name!r}, "
+            f"REF_COL_SEQ_NO={self.ref_col_seq_no!r}, "
+            f"REMARK={self.remark!r}, "
+            f"{super().__repr__()}"
+            " )"
+        )
+
+    def to_string(self, use_verbose: bool, last_record: Self | None = None) -> str:
+        base_string = f"{self.nav_id} :: {self.nav_type}"
+
+        modification_string = ""
+        if last_record:
+            modification_string = f" :: {self.get_mod_string(last_record)}"
+
+        record_string = ""
+        if use_verbose:
+            record_string = (
+                " :: [ "
+                f"EFF_DATE: {self.eff_date}, "
+                f"STATE_CODE: {self.state_code}, "
+                f"CITY: {self.city}, "
+                f"COUNTRY_CODE: {self.country_code}, "
+                f"TAB_NAME: {self.tab_name}, "
+                f"REF_COL_NAME: {self.ref_col_name}, "
+                f"REF_COL_SEQ_NO: {self.ref_col_seq_no}, "
+                f"REMARK: {self.remark}"
+                " ]"
+            )
+
+        return f"{base_string}{modification_string}{record_string}"
+
+
+@register_faa_file("NAV_RMK")
+class NAV_RMK_File(FAA_File_Base):
+    def __init__(
+        self,
+        file_path: str,
+        use_verbose: bool,
+        filter_object: FilterObject | None = None,
+    ) -> None:
+        super().__init__(file_path, "Navigation Remark", use_verbose, filter_object)
+
+        self.__load_from_csv()
+
+    def __load_from_csv(self) -> None:
+        with open(self.file_path, "r") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                record = NAV_RMK(
+                    eff_date=row["EFF_DATE"],
+                    nav_id=row["NAV_ID"],
+                    nav_type=row["NAV_TYPE"],
+                    state_code=row["STATE_CODE"],
+                    city=row["CITY"],
+                    country_code=row["COUNTRY_CODE"],
+                    tab_name=row["TAB_NAME"],
+                    ref_col_name=row["REF_COL_NAME"],
+                    ref_col_seq_no=row["REF_COL_SEQ_NO"],
+                    remark=row["REMARK"],
+                    file=row["File"],
+                    action=Action(row["Action"]),
+                    mods=row["Mods"],
+                )
+
+                use_filters = True if self.filter_object else False
+                is_in_filters = False
+                if use_filters and self.filter_object is not None:
+                    # This file does not have filterable fields.
+                    pass
+
+                if not use_filters or is_in_filters:
+                    if record.action == Action.ADDED:
+                        self.adds.append(record)
+                    if record.action == Action.MODIFIED:
+                        self.mods.append(record)
+                    if record.action == Action.DELETED:
+                        self.dels.append(record)
